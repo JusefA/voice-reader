a51edb99db4854ab6fa8cd0bcd02ad7284e97d6d
Jusef Al-itawa <jusef03@gmail.com>
2023-01-04 16:17:05 +0200

    Add project files.

---
+++ voice reader/voice_reader.py
@@ -0,0 +1,11 @@
+﻿from gtts import gTTS
+import os
+#text reader for arabic language
+
+MyText = "كل يوم ق لاللهم أِنَّ كُلًَ عَمَلٍ أَعمَلهُ من الواجب والمَندوب* وكُلَّ تَركٍ أَترُكُهُ من الحرام والمكروه كُلهُ قُربَةً لِوَجهِكَ الكَريم»"
+Language = "ar"
+
+output = gTTS(text=MyText, lang=Language, slow=False)
+
+output.save("texttospeec.mp3")
+os.system("start texttospeec.mp3")
